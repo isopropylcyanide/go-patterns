e713b2766186ec79b68a8ee533d818d917377791
Aman Garg <amangarg1995sep@gmail.com>
2021-04-27 00:40:39 +0530

    Add For Select loop

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # go-patterns
-Various patterns and dabble in Golang
+Various patterns and dabble in Golang.
+
+## Sources
+- [Concurrency in Go](https://www.oreilly.com/library/view/concurrency-in-go/9781491941294/)
+- [Hands on Go Programming](https://books.google.co.in/books/about/Hands_on_Go_Programming.html?id=Q3whEAAAQBAJ&redir_esc=y)
+
+
+| Pattern | Description | 
+| --- | --- |
+| Confinement | Safe concurrent access without channels or mutex |
+| For Select | Common idioms when dealing with multiple channels |
+
+
---
+++ forSelectLoop/forSelectLoop.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//Common patterns involving for/select
+func sendIterationValuesOnChannel() {
+	done := make(chan struct{})
+	stringStream := make(chan string, 3)
+
+	go func() {
+		for _, v := range []string{"A", "B", "C"} {
+			select {
+			case <-done:
+				return
+			case stringStream <- v:
+			}
+		}
+		close(stringStream)
+	}()
+
+	for str := range stringStream {
+		fmt.Println("Got ", str)
+	}
+}
+
+func infiniteLooping() {
+	done := make(chan struct{})
+	for {
+		select {
+		case <-done:
+			//until done is passed a value, this will exit the select block and loop
+			return
+		default:
+		}
+		//do work here
+		fmt.Println("Looping")
+	}
+}
+
+func infiniteLoopingII() {
+	done := make(chan struct{})
+	for {
+		select {
+		case <-done:
+			return
+		case <-time.After(50 * time.Millisecond):
+			fmt.Println("okay")
+			return
+		default:
+			//do work here in default (also an option) however aim for less indentation
+			fmt.Println("Looping II")
+		}
+	}
+}
+
+func main() {
+	sendIterationValuesOnChannel()
+	//this will loop infinitely
+	infiniteLooping()
+	//this will loop infinitely as well
+	infiniteLoopingII()
+}
